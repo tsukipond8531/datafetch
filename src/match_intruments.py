16e8b35a8db4daa323867f4f6ccc1cd730199bbc
tsukipond8531 <reader2027@gmail.com>
2023-11-29 14:44:46 -0300

    moving match instruments

---
+++ src/match_intruments.py
@@ -0,0 +1,26 @@
+import os
+import imager as im 
+import digisonde as dg
+
+
+
+
+def get_closest_iono(fn_sky, iono_dir):
+        
+    sky_dt = im.imager_fname(fn_sky).datetime
+    
+    files = os.listdir(iono_dir)
+    
+    iono_files = sorted(
+        [dg.ionosonde_fname(fn) 
+         for fn in files if 'PNG' in fn]
+        )
+    
+    dn = min(
+        iono_files, 
+        key = lambda x: abs(x - sky_dt)
+        )
+    
+    code = files[0].split('_')[0]
+        
+    return dn.strftime(f"{code}_%Y%m%d(%j)%H%M%S.PNG")
