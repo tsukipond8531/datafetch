6a171440f08e05063d7d3156ce6352d8aa1f5590
tsukipond8531 <reader2027@gmail.com>
2024-05-25 15:01:58 +0900

    new copy files function

--- src/digisonde_seek.py
+++ src/digisonde_seek.py
@@ -1,82 +1,21 @@
 import os 
-import Webscrape as wb 
-from base import make_dir
 import shutil
-import GNSS as gs
 from tqdm import tqdm 
+import base as b 
 
 
-def unzip_and_convert(dst):
+infile = 'D:/ionograms/07/'
+
+
+for folder in tqdm(os.listdir(infile)):
     
-    for fname in os.listdir(dst):
-        print('[unziping]', fname)
+    path_out = os.path.join(infile, folder[-1])
+    b.make_dir(path_out)
+    path_folder = os.path.join(infile, folder)
+    
+    for file in os.listdir(path_folder):
+        src = os.path.join(path_folder, file)
+        dst = os.path.join(path_out, file)
         
-        try:
-            wb.unzip_orbit(dst + fname)
-          
-        except:
-            pass
-    
-    for file in os.listdir(dst):
-        print('[converting]', file)
-        wb.crx2rnx(dst + file)
- 
-
-
-def get_year_doy(filename):
-    ars = filename.split('.')
-    year = int('20' + ars[1][:2])
-    doy = int(ars[0][4:-1])
-
-    return year, doy
-
-
-def copy_from_dst(src):
-
-    for filename in os.listdir(src):
-    
-        year, doy = get_year_doy(filename)
-        
-        dst = gs.paths(year, doy).rinex
-        make_dir(dst)
-        print('[copying]', filename)
-        shutil.copy(src + filename, 
-                    os.path.join(dst, filename))
-        
-
-
-def get_sao_date(fname):
-    
-    arg = fname.split('_')[-1][:-4]
-    
-    year = int(arg[:4])
-    doy = int(arg[4:])
-    return gs.date_from_doy(year, doy)
-
-
-    
-yr = 2014
-    
-src = f'D:\\database\\jic\\sao{yr}\\'
-dst = 'D:\\database\\jic\\temp\\'
-
-def run_files(month_path, dst ):
-
-    for filename in os.listdir(month_path):
-        src_in = os.path.join(month_path, filename)
-        shutil.copy(src_in, dst)
-
-for folder in tqdm(os.listdir(src), str(yr)):
-    
-    if folder == '02':
-        pass
-    else:
-        month_path = os.path.join(
-            src, folder
-            )
-        
-        run_files(month_path, dst)
-            
-        
-                
- 
+        if file.endswith('SAO'):
+            shutil.copy(src, dst)
